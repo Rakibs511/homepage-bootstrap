ea4097d327e54b3721dc00c804e0d33a93302c62
Rakibul Hasan <rakibulhasan20162@gmail.com>
2023-07-01 16:59:07 +0600

    initialize bootstrap project with vite

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+const path = require('path')
+
+export default {
+  root: path.resolve(__dirname, 'src'),
+  base:'/homepage-bootstrap',
+  build: {
+    outDir: '../dist'
+  },
+  server: {
+    port: 8080
+  }
+}
